262782c8b63d06bae4cb9353c2160c5b9cf2a0b4
Keegan Donley <keegan.donley@gmail.com>
2017-05-09 18:46:24 -0700

    Add readme and more docs

---
+++ README.md
@@ -0,0 +1,22 @@
+## CS 315 Project 5
+# Keegan Donley
+# 5/9/2017
+
+Everything works in this project as needed in the project specifications.
+
+The only difference, is my worm starts with the head on the left hand side instead
+of on the right.
+
+
+The worm moves in the following way:
+h: LEFT
+j: DOWN
+k: UP
+l: RIGHT
+
+
+The worm is initialized with a head and 7 body segments in the center of the screen.
+
+Hitting yourself (the worm) or the border will cause the game to end.
+
+The score is the combined value of 'munchies' that are picked up by moving on top of them.
--- board.hpp
+++ board.hpp
@@ -6,55 +6,70 @@
 
 class Board {
 public:
+    // Initilizes the board
     Board (int row, int col);
-    void print();
+
+    // Struct used to keep track of all points
     struct coord {
         int x;
         int y;
     };
+
+    // Get information about the gurrent game
     int getMatrixVal(int x, int y) { return board[x][y]; }
     void removeFromFree(int x, int y);
     void addToFree(int x, int y);
     int  getLastIdx() { return lastIdx; }
+
+    // Maintain the corrdinates of the current positions
     coord getCoords(int idx) { return freepool[idx]; }
     coord& getCurrent() { return currentPos; }
     coord& getPrevious() { return previousPos; }
+
+    // Retrieve and set munchie and score data
     coord& getMunchie() { return currentMunchiePos; }
     bool& isLiveMunchie() { return liveMunchie; }
     int& getMunchieValue() { return munchieValue; }
     int& getMunchieCountdown() { return munchieCountdown; }
     int& getScore() { return totalScore; }
+
+    // Retrieves and sets the win condition
     bool& isWin() { return win; }
 
+    // Manipulates the worm data
     void setWorm(int numRows, int numCols);
-    void setHeadAndTail(int h, int t) {head = h; tail = t; }
     bool growWorm(char direction);
     bool moveWorm(char direction);
-    int getNextIdx() { return tail + 1; }
     int moveTail();
     int moveHead(coord current);
 
 
 private:
 
+    // General game structure
     int     lastIdx;
     int     **board;
     coord   *freepool;
+
+    // Data for the worm
     coord   *worm;
     int     wormLastIdx;
-
     int     head;
     int     tail;
 
+    // Data for the munchie
     bool    activeMunchie;
     bool    liveMunchie;
     int     munchieValue;
     int     munchieCountdown;
+
+    // Data for the current state
     int     totalScore;
     coord   currentPos;
     coord   previousPos;
     coord   currentMunchiePos;
 
+    // Tracks if the program terminated due to a win or loss
     bool    win;
 
 };
